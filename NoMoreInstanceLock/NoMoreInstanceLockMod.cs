4cbe2c408677271bf64688d5462610fb6081035d
MajoraDev <syphe@DESKTOP-A5A8VR6>
2022-05-27 11:29:35 +0200

    Add project files.

---
+++ NoMoreInstanceLock/NoMoreInstanceLockMod.cs
@@ -0,0 +1,18 @@
+﻿using MelonLoader;
+using NoMoreInstanceLock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: MelonInfo(typeof(NoMoreInstanceLockMod), "No More Instance Lock", "1.0.0", "Majora")]
+[assembly: MelonGame("VRChat", "VRChat")]
+
+namespace NoMoreInstanceLock
+{
+    public class NoMoreInstanceLockMod
+    {
+
+    }
+}
